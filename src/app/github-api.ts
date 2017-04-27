a0cffefe64bc0a553c635135658f9cafed11a2f6
mkinitcpio <wot220697@gmail.com>
2017-04-27 19:20:32 +0300

    Refactored github-api class

--- src/app/github-api.ts
+++ src/app/github-api.ts
@@ -14,18 +14,14 @@ export class GitHubApi {
     private GITHUB_URL = "https://api.github.com";
     private _client: any;
 
-    constructor(private _http: Http) {
-
-    }
+    constructor(private _http: Http) { }
 
     public searchRepos(reg: string): Promise<Repository[]> {
-        return this._http.get(`${this.GITHUB_URL}/search/repositories?q=${reg}`).map((responce: any) => {
-            let body: any = JSON.parse(responce._body);
-            console.log(body.items);
+        return this._http.get(`${this.GITHUB_URL}/search/repositories?q=${reg}`).map(responce => {
+            let body = responce.json();
             return body.items;
-        }).map(reposData => {
-            let searchedRepos: Repository[] = [];
-            reposData.map((data: any) => {
+        }).map((reposData) => {
+            return reposData.map((data: any) => {
                 let searchedRepo: Repository = new Repository();
                 searchedRepo.name = data.name;
                 searchedRepo.fullname = data.full_name;
@@ -36,23 +32,19 @@ export class GitHubApi {
                 owner.accountUrl = data.owner.html_url;
                 owner.avatarUrl = data.owner.avatar_url;
                 searchedRepo.owner = owner;
-                searchedRepo.lastCommitKey= "";
+                searchedRepo.lastCommitKey = "";
                 searchedRepo.commits = [];
-                
-                searchedRepos.push(searchedRepo);
+                return searchedRepo;
             });
-
-            return searchedRepos;
         }).toPromise();
     }
 
     public getRepositoryCommits(repositoryFullname: string): Promise<any> {
-        return this._http.get(`${this.GITHUB_URL}/repos/${repositoryFullname}/commits?client_id=${this.ID}&client_secret=${this.SECRET}`).map((response: any) => {
-            let commitsData = JSON.parse(response._body);
+        return this._http.get(`${this.GITHUB_URL}/repos/${repositoryFullname}/commits?client_id=${this.ID}&client_secret=${this.SECRET}`).map(response => {
+            let commitsData = response.json();
             return commitsData;
         }).map((commitsData: any) => {
-            let repositoryCommits: Commit[] = [];
-            commitsData.map((data: any) => {
+            return commitsData.map((data: any) => {
                 let commit = new Commit();
                 commit.key = data.sha;
                 commit.message = data.commit.message;
@@ -64,9 +56,8 @@ export class GitHubApi {
                 author.accountUrl = data.committer.html_url;
                 author.avatarUrl = data.committer.avatar_url;
                 commit.author = author;
-                repositoryCommits.push(commit);
+                return commit;
             });
-            return repositoryCommits;
         }).toPromise();
     }
 
