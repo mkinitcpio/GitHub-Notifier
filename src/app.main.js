006354623556c057812e99becfac816fe9064aa5
mkinitcpio <wot220697@gmail.com>
2017-05-12 09:19:45 +0300

    Added hide to tray support

--- src/app.main.js
+++ src/app.main.js
@@ -7,8 +7,8 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
 let mainWindow
 
@@ -22,29 +22,58 @@ function createWindow() {
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   tray = new Tray('github-notifier-icon.png');
   tray.on('click', () => {
-    win.isVisible() ? mainWindow.hide() : mainWindow.show()
+    win.isVisible() ? mainWindow.hide() : mainWindow.show();
   });
+
+  let contextMenu = Menu.buildFromTemplate([{
+      label: 'Show App',
+      click: function () {
+        mainWindow.show();
+      }
+    },
+    {
+      label: 'Quit',
+      click: function () {
+        app.isQuiting = true;
+        app.quit();
+      }
+    }
+  ]);
+
+  tray.setContextMenu(contextMenu);
 
   mainWindow.setMenuBarVisibility(false);
   mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
+
+  mainWindow.on('minimize', function (event) {
+    event.preventDefault()
+    mainWindow.hide();
+    mainWindow.on('close', function (event) {
+      if (!app.isQuiting) {
+        event.preventDefault();
+        mainWindow.hide();
+      }
+      return false;
+    });
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
 app.on('activate', function () {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
 })
